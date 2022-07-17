80db92ab1cb4eabd9e074a3db37b5d516dbe3ce6
fluffyhorse <loveforever29@yandex.ru>
2022-07-17 13:33:17 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,8 @@
 function start(){
   exe=1;
   wood=0;
-  document.write('<link rel="stylesheet" href="style.css"><center><button onclick="game();">Игра</button><button onclick="invy();">Инветнарь</button><button onclick="create();">Рецепты</button><div id="windows"><img  onclick="add_wood();" src="img/Куст.png" width="100" height="100"></div></center>');
+  stone=0;
+  document.write('<link rel="stylesheet" href="style.css"><center><button onclick="game();">Игра</button><button onclick="invy();">Инветнарь</button><button onclick="create();">Рецепты</button><div id="windows"><table><tr><td><img  onclick="add_wood();" src="img/Куст.png" width="100" height="100"></td><td><img  onclick="add_stone();" src="img/Камень.png" width="100" height="100"></td></tr></div></center>');
 }
 function add_wood(){
   if(exe==2){
@@ -11,13 +12,16 @@ function add_wood(){
     wood=wood+1;
   }
 }
+function add_stone(){
+  stone=stone+1;
+}
 function game(){
-  windows.innerHTML='<img  onclick="add_wood();" src="img/Куст.png" width="100" height="100">';
+  windows.innerHTML='<table><tr><td><img  onclick="add_wood();" src="img/Куст.png" width="100" height="100"></td><td><img  onclick="add_stone();" src="img/Камень.png" width="100" height="100"></td></tr>';
 }
 function invy(){
-  windows.innerHTML="Дерево:"+ wood;
+  windows.innerHTML="Дерево:"+ wood+"Камень"+stone;
   if(exe==2){
-    windows.innerHTML="Дерево:"+ wood+"<p>топор</p>";
+    windows.innerHTML="Дерево:"+ wood+"Камень"+stone+"<p>топор</p>";
   }
     
 }
